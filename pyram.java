d0f690a1998ce054690cfa1203ccbcc0d3219900
23Adarsh <107578738+23Adarsh@users.noreply.github.com>
2022-07-23 00:39:26 +0530

    Add files via upload

---
+++ pyram.java
@@ -0,0 +1,25 @@
+
+  import java.util.Scanner;
+  public class pyram{
+      public static void main(String[] args)
+      {
+          int rows;
+          Scanner s = new Scanner(System.in);
+          
+          System.out.print("Enter Value of Rows -:");
+          rows = s.nextInt();
+          
+          for(int i=1;i<rows;i++)
+          {
+              for(int j=1;j<rows-i;j++)
+              {
+                  System.out.print(" ");
+              }
+              for(int k=1;k<=i;k++)
+              {
+                  System.out.print(i+" ");
+              }
+              System.out.println();
+          }
+      }
+  }
